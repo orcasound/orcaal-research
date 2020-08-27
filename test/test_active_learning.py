61ab20152e1f19911e55fdd0639ee09cfcbd1ea1
kunal mehta <36035718+kunakl07@users.noreply.github.com>
2020-08-27 19:35:44 +0530

    Update test_active_learning.py
    Directly run from main

--- test/test_active_learning.py
+++ test/test_active_learning.py
@@ -3,10 +3,13 @@ import active_learning
 import numpy.testing as npt
 
 
-class TestPreprocess(unittest.TestCase):
+class TestAL(unittest.TestCase):
 
     def test_active_learning(self):
         probability_last, class_ten = active_learning.predict("preprocess_mag_scipy_Srkws.h5", "test/calls/")
 
         self.assertEqual(class_ten, 0)
         npt.assert_almost_equal(probability_last, 0.5279, decimal=3)
+
+if __name__ == '__main__'
+    unittest.main()
