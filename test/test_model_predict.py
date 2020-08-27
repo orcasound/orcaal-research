a98355b450748ff77ff7dff5bc119366ed2d7410
kunal mehta <36035718+kunakl07@users.noreply.github.com>
2020-08-27 19:36:24 +0530

    Update test_model_predict.py

--- test/test_model_predict.py
+++ test/test_model_predict.py
@@ -2,8 +2,12 @@ import unittest
 import model_predict
 
 
-class TestPreprocess(unittest.TestCase):
+class Testpredict(unittest.TestCase):
     def test_predict(self):
         no_of_predicted_calls, shape = model_predict.predict("preprocess_mag_scipy_Srkws.h5", "content/datasets/val_srkw/calls")
         self.assertEqual(no_of_predicted_calls, 44)
         self.assertEqual(shape, (44, 1))
+
+
+if __name__ == '__main__'
+    unittest.main()
