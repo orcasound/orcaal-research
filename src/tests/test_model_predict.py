64d1bb5f624dfbcdc9743926d39bedf61ef49191
kunal mehta <36035718+kunakl07@users.noreply.github.com>
2020-08-31 08:42:26 +0530

    Update test_model_predict.py

--- src/tests/test_model_predict.py
+++ src/tests/test_model_predict.py
@@ -7,7 +7,7 @@ from model_predict import predict
 
 class TestPreprocess(unittest.TestCase):
     def test_predict(self):
-        no_of_predicted_calls, shape = predict("srkw_cnn.h5", "../datasets/test_srkw/calls")
+        no_of_predicted_calls, shape = predict('srkw_cnn.h5', '../datasets/test_srkw/calls')
         self.assertEqual(no_of_predicted_calls, 3)
         self.assertEqual(shape, (3, 1))
 
