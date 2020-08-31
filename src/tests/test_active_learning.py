aa20a6fb943255103bf2088f8cf679028736f2a0
kunal mehta <36035718+kunakl07@users.noreply.github.com>
2020-08-31 08:41:03 +0530

    Update test_active_learning.py

--- src/tests/test_active_learning.py
+++ src/tests/test_active_learning.py
@@ -9,7 +9,7 @@ import numpy.testing as npt
 class TestPreprocess(unittest.TestCase):
 
     def test_active_learning(self):
-        probability_last, class_ten = predict("srkw_cnn.h5", "../datasets/test_srkw/calls/")
+        probability_last, class_ten = predict('srkw_cnn.h5', '../datasets/test_srkw/calls/')
         self.assertEqual(class_ten, 0)
         npt.assert_almost_equal(probability_last, 0.5000879, decimal=3)
 
